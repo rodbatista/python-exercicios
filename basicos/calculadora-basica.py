b55f7b16b5f59731130ae80cec408570c9588247
rodrigo <rodrigobc032008@gmail.com>
2026-03-04 23:38:40 -0300

    automação das operaçoes matematicas

--- basicos/calculadora-basica.py
+++ basicos/calculadora-basica.py
@@ -1,6 +1,20 @@
-num = int(input("Digite um numero:"))
+opera_valida = ['x','-','+','/']
+num1 = int(input("Digite o primeiro numero:"))
 num2 = int(input("Digite o segundo numero:"))
-print(f"A soma dos dois numeros {num, num2} foi ", num + num2)
-print(f"A multiplicação dos dois numeros {num, num2} foi ", num * num2)
-print(f"A divisão dos dois numeros {num, num2} foi ", num / num2)
-print(f"A subtração dos dois numeros {num, num2} foi ", num - num2)
+
+
+while True:
+    opera = input('Qual a operação? (x,-,+,/)')
+    if opera in opera_valida:
+        break
+    else:
+        print("operação invalida, tente novamente.")
+
+if opera == 'x':
+    print(f"A multiplicação é: {num1 * num2}")
+elif opera == '-':
+    print(f"A subtração é: {num1 - num2}")
+elif opera == '+':
+    print(f"A soma é: {num1 + num2}")
+elif opera == '/':
+    print(f"A divisao é: {num1 / num2}")
