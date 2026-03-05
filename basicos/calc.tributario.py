41a15c41e242e01c8ca55f82a2120a9d51cfaef3
rodrigo <rodrigobc032008@gmail.com>
2026-03-05 10:46:06 -0300

    add def para codigo mais limpo e funcional

--- basicos/calc.tributario.py
+++ basicos/calc.tributario.py
@@ -2,7 +2,16 @@
 # 2000 a 5000 -> 10%
 # acima de 5000 -> 20%
 
-opcao_valida = ['1','2','3','4']
+def calcular_imposto(renda):
+    if 0 < renda <= 2000:
+        return 0
+    elif 2000 < renda <= 5000:
+        return 0.10
+    else:
+        return 0.20
+
+
+opcao_valida = ['1', '2', '3', '4']
 
 while True:
     print("\n=== SISTEMA DE CÁLCULO DE IMPOSTO ===")
@@ -11,56 +20,29 @@ while True:
     print("3 - Simular imposto anual")
     print("4 - Sair")
 
-
     opcao = input("Escolha uma das opções acima:")
-    if opcao not in opcao_valida:   #SO ACONTECE SE ISSO FOR VERDADEIRO.
+    if opcao not in opcao_valida:  #SO ACONTECE SE ISSO FOR VERDADEIRO.
         print("Digite novamente uma opção válida!")
-        continue   #SE FOR TRUE, ELE CONTINUA O LOOPING VOLTANDO PRO TOPO, O BREAK PARA O LOOPING.
+        continue  #SE FOR TRUE, ELE CONTINUA O LOOPING VOLTANDO PRO TOPO, O BREAK PARA O LOOPING.
+
+    if opcao == '4':
+        print("Finalizando processo...")
+        break
+
+    renda = float(input("Digite sua renda mensal:"))
+    aliquota = calcular_imposto(renda)
+    imposto = aliquota * renda
 
     if opcao == '1':
-        renda = float(input("Digite sua renda mensal:"))
-        if 0 < renda <= 2000:
-            print(f"Para salarios até R$2000.00, é aplicada uma aliquota nula.")
-        elif 2000 < renda <= 5000:
-            print(f"Para salarios acima de R$2000.00 e abaixo ou igual a R$5000.00, aplica-se uma aliquota de 10%")
-            aliquota = 0.10
-        else:
-            print(f"Para salarios acima de R$5000.00, aplica-se uma aliquota de 20%.")
-            aliquota = 0.20
-
-        imposto = round(aliquota * renda,2 )
-        print(f"A aliquota aplicada é de {aliquota * 100}%")
-        print(f"O imposto devido é de: {imposto}")
+        print(f"Aliquota aplicada: {aliquota}")
+        print(f"Imposto devido: {imposto}")
 
     elif opcao == '2':
-        renda = float(input("Digite sua renda mensal:"))
-        if 0 < renda <= 2000:
-            print(f"Para salarios até R$2000.00, é aplicada uma aliquota nula.")
-        elif 2000 < renda <= 5000:
-            print(f"Para salarios acima de R$2000.00 e abaixo ou igual a R$5000.00, aplica-se uma aliquota de 10%")
-            aliquota = 0.10
-        else:
-            print(f"Para salarios acima de R$5000.00, aplica-se uma aliquota de 20%.")
-            aliquota = 0.20
-
-        imposto = round(aliquota * renda,2 )
-        print(f"O imposto devido é de: {imposto}")
-        print(f"A sua renda liquida é de: {renda - imposto}")
+        print(f"Aliquota aplicada: {aliquota}")
+        print(f"Renda liquida de: {renda - (imposto)}")
 
     elif opcao == '3':
-        renda = float(input("Digite sua renda mensal:"))
-        if 0 < renda <= 2000:
-            print(f"Para salarios até R$2000.00, é aplicada uma aliquota nula.")
-        elif 2000 < renda <= 5000:
-            print(f"Para salarios acima de R$2000.00 e abaixo ou igual a R$5000.00, aplica-se uma aliquota de 10%")
-            aliquota = 0.10
-        else:
-            print(f"Para salarios acima de R$5000.00, aplica-se uma aliquota de 20%.")
-            aliquota = 0.20
+        print(f"Aliquota aplicada: {aliquota}")
+        print(f"Imposto anual é de: {aliquota * (renda * 12)}")
 
-        imposto_anual = round(aliquota * (renda * 12), 2)
-        print(f"A sua renda anual é de: {renda * 12}")
-        print(f"O imposto devido anual é de: {imposto_anual}")
 
-    elif opcao == '4':
-        break
